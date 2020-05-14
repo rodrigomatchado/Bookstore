e5df5a59883c27c23b3d7d6359681fb1c8320fef
rodrigo machado <rodrigomachado21@hotmail.com>
2020-05-14 13:47:56 +0100

    Teste2

--- Bookstore/Startup.cs
+++ Bookstore/Startup.cs
@@ -16,7 +16,7 @@ namespace Bookstore
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc();
+            services.AddControllersWithViews();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -31,10 +31,7 @@ namespace Bookstore
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
+                endpoints.MapDefaultControllerRoute();
             });
         }
     }
